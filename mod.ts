b2c37c48decb621eaf1ce003e1011d04ab340fbf
linux_china <libing.chen@gmail.com>
2022-06-22 18:49:25 -0700

    feat: export myip()

--- mod.ts
+++ mod.ts
@@ -1,6 +1,5 @@
-import {instantiate, hello, add} from "./lib/wasm_demo.generated.js";
+import {instantiate, hello, add, myip} from "./lib/wasm_demo.generated.js";
 // initialize Wasm
 await instantiate();
 
-export {add, hello};
-
+export {add, hello, myip};
