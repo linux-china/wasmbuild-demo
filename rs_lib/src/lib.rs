795d7995f012cf7c9f9dfabdc75d6447738a3cbc
linux_china <libing.chen@gmail.com>
2022-06-22 18:48:27 -0700

    feat: add myip to call http service

--- rs_lib/src/lib.rs
+++ rs_lib/src/lib.rs
@@ -1,4 +1,10 @@
 use wasm_bindgen::prelude::*;
+use serde::{Deserialize, Serialize};
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{Request, RequestInit, RequestMode, Response};
+
 
 #[wasm_bindgen]
 pub fn add(a: i32, b: i32) -> i32 {
@@ -9,6 +15,29 @@ pub fn add(a: i32, b: i32) -> i32 {
 pub fn hello(name: &str) -> String {
     return format!("Hello, {}!", name);
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct MyIp {
+    pub origin: String,
+}
+
+#[wasm_bindgen]
+pub async fn myip() -> Result<String, JsValue> {
+    let mut opts = RequestInit::new();
+    opts.method("GET");
+    let url = "https://httpbin.org/ip";
+    let request = Request::new_with_str_and_init(&url, &opts)?;
+    let window = web_sys::window().unwrap();
+    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
+    // `resp_value` is a `Response` object.
+    let resp: Response = resp_value.dyn_into().unwrap();
+    // Convert this other `Promise` into a rust `Future`.
+    let json = JsFuture::from(resp.json()?).await?;
+    // Use serde to parse the JSON into a struct.
+    let my_ip: MyIp = json.into_serde().unwrap();
+    Ok(my_ip.origin)
+}
+
 
 #[cfg(test)]
 mod tests {
